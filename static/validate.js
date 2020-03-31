2189be0219646dae4fd392bf60cf447185ce94f0
nasim <nasim.uddin@graaho.com>
2020-03-31 23:27:30 +0600

    Footer position issue solve

--- static/validate.js
+++ static/validate.js
@@ -2,16 +2,18 @@ form = document.querySelector('form')
 container = document.querySelector('.container')
 popup = document.querySelector('.popup')
 quitButton = document.querySelector('.quit')
+footer = document.querySelector('footer')
 
 form.addEventListener('submit', function(e) {
   uppercase = document.querySelector('#uppercase').checked
   lowercase = document.querySelector('#lowercase').checked
   number = document.querySelector('#number').checked
   symbol = document.querySelector('#symbol').checked
-  console.log(uppercase || lowercase || number || symbol)
+
   if (!(uppercase || lowercase || number || symbol)) {
     e.preventDefault()
     container.classList.add('hide')
+    footer.style.paddingTop = '1rem'
     popup.classList.remove('hide')
   }
 })
@@ -19,5 +21,6 @@ form.addEventListener('submit', function(e) {
 // popup close function
 quitButton.addEventListener('click', function() {
   container.classList.remove('hide')
+  footer.style.paddingTop = '0'
   popup.classList.add('hide')
 })
